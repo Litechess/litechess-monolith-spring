1346a32b4e5cc4cf555a5770ca0832f0af2a8b0c
Trymad <wolfesss2015@gmail.com>
2025-08-12 13:12:05 +0300

    refactor chesspartyservice doMove method

--- src/main/java/com/trymad/litechess_monolith/chessgame/internal/service/ChessUtilService.java
+++ src/main/java/com/trymad/litechess_monolith/chessgame/internal/service/ChessUtilService.java
@@ -3,11 +3,14 @@ package com.trymad.litechess_monolith.chessgame.internal.service;
 import java.util.List;
 
 import com.trymad.litechess_monolith.chessgame.GameMove;
+import com.trymad.litechess_monolith.websocket.MoveRequest;
 
 public interface ChessUtilService {
 	
 	String getSan(String fen, List<GameMove> moves);
 
 	String getFen(String fen, List<GameMove> moves);
+
+	GameMove toGameMove(MoveRequest request);
 
 }
