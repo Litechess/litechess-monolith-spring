154cb267b3ec87ba237f514985ad1374f85af253
Trymad <wolfesss2015@gmail.com>
2025-10-03 03:14:20 +0300

    add corse rules, fix livegame controller

--- src/main/java/com/trymad/litechess_monolith/livegame/internal/controller/LiveGameController.java
+++ src/main/java/com/trymad/litechess_monolith/livegame/internal/controller/LiveGameController.java
@@ -3,7 +3,9 @@ package com.trymad.litechess_monolith.livegame.internal.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +20,14 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/livegames")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LiveGameController {
 	
 	private final LiveGameMapper mapper;
 	private final LiveGameService liveGameService;
 
 	@GetMapping("/{id}")
-	LiveGameDTO getById(Long id) {
+	LiveGameDTO getById(@PathVariable Long id) {
 		return mapper.toDto(liveGameService.get(id));
 	}
 
