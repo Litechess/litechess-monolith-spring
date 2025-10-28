39863ef4928ce281c0c55a2716f5f1680b8c5001
Trymad <wolfesss2015@gmail.com>
2025-10-28 14:45:43 +0300

    fix bug when game no time controll

--- src/main/java/com/trymad/litechess_monolith/livegame/internal/service/LiveGameService.java
+++ src/main/java/com/trymad/litechess_monolith/livegame/internal/service/LiveGameService.java
@@ -14,6 +14,7 @@ import com.trymad.litechess_monolith.chessparty.api.event.MoveAcceptedEvent;
 import com.trymad.litechess_monolith.chessparty.api.model.ChessGameStatus;
 import com.trymad.litechess_monolith.chessparty.api.model.GameMove;
 import com.trymad.litechess_monolith.chessparty.api.model.PlayerColor;
+import com.trymad.litechess_monolith.chessparty.api.model.TimeControlType;
 import com.trymad.litechess_monolith.livegame.api.event.GameFinishEvent;
 import com.trymad.litechess_monolith.livegame.api.event.LiveGameStartEvent;
 import com.trymad.litechess_monolith.livegame.internal.controller.filter.LiveGameFilter;
@@ -50,15 +51,18 @@ public class LiveGameService  {
 			throw new IllegalStateException("Can't create live game for finished party: " + chessParty.id());
 		}
 
-		final GameTimer gameTimer = 
+		final GameTimer gameTimer = chessParty.timeControl().type() == TimeControlType.NO_CONTROL ? null :
 			gameTimeService.createTimer(chessParty.timeControl(), new TimerHistory(chessParty.timerHistory()));
 		
 		final LiveGame newLiveGame = new LiveGame(chessParty, gameTimer);
 		emulatorService.createEmulator(chessParty.id()).setPosition(chessParty.moves());
 
 		final LiveGame gameFromRepo = liveGameRepository.save(newLiveGame);
-		gameTimer.start();
-		gameTimeService.startTimer(chessParty.id(), gameTimer, whenTimeout(chessParty.id(), gameTimer));
+
+		if(gameTimer != null) {
+			gameTimer.start();
+			gameTimeService.startTimer(chessParty.id(), gameTimer, whenTimeout(chessParty.id(), gameTimer));
+		}
 
 
 		final LiveGameStartEvent event = new LiveGameStartEvent(liveGameMapper.toDto(gameFromRepo));
