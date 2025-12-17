4fc40c971e7f4b63730c45fd2ef8c9f201030ddd
Trymad <wolfesss2015@gmail.com>
2025-12-17 23:13:38 +0300

    delete unused import

--- src/main/java/com/trymad/litechess_monolith/LitechessMonolithApplication.java
+++ src/main/java/com/trymad/litechess_monolith/LitechessMonolithApplication.java
@@ -3,11 +3,8 @@ package com.trymad.litechess_monolith;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import lombok.RequiredArgsConstructor;
 
