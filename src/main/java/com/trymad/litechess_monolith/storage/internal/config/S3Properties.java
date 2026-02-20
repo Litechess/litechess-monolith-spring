bb4547457a87c2310b8d53938569039ecb11a275
Trymad <wolfesss2015@gmail.com>
2026-02-20 20:01:32 +0300

    migrate to subdomain model

--- src/main/java/com/trymad/litechess_monolith/storage/internal/config/S3Properties.java
+++ src/main/java/com/trymad/litechess_monolith/storage/internal/config/S3Properties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "s3")
 public record S3Properties(
-	String endpoint,
+	String url,
 	String accessKey,
 	String secretKey,
 	String region,
